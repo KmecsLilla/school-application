e35b7a75ae7a3d66bac8d0169dd81269e6ee7078
KmecsLilla <kmecs.lilla@gmail.com>
2021-08-10 17:31:25 +0200

    Add initial data to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO School(name, address) VALUES ('Petofi Sandor Altalanos Iskola', 'Kiskoros, Petofi utca 3.');
+INSERT INTO School(name, address) VALUES ('Arany Janos Altalanos Iskola', 'Nagykoros, Hosok tere 8.');
+INSERT INTO School(name, address) VALUES ('Arany Janos Gimnazium', 'Nagykoros, Hosok tere 6.');
+
+INSERT INTO Division(name, class_master, startup_year, number_of_members, school_id) VALUES('1.b', 'Kovacs Maria', 2020, 26, 2);
+INSERT INTO Division(name, class_master, startup_year, number_of_members, school_id) VALUES('1.c', 'Horvath Maria', 2020, 21, 2);
+INSERT INTO Division(name, class_master, startup_year, number_of_members, school_id) VALUES('2.b', 'Kiss Jozsef', 2019, 23, 2);
+INSERT INTO Division(name, class_master, startup_year, number_of_members, school_id) VALUES('1.b', 'Szabo Maria', 2020, 26, 1);
